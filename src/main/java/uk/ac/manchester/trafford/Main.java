87cab79ef98e3f47bcceefa64553a3e5b56afedf
ricbir <ricbir@DESKTOP-9R5074T.home>
2018-09-24 19:21:51 +0100

    Initial commit

---
+++ src/main/java/uk/ac/manchester/trafford/Main.java
@@ -0,0 +1,6 @@
+package uk.ac.manchester.trafford;
+public class Main {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/uk/ac/manchester/trafford/MainTest.java
@@ -0,0 +1,9 @@
+package uk.ac.manchester.trafford;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test public void testSomeLibraryMethod() {
+        fail("not implemented");
+    }
+}
