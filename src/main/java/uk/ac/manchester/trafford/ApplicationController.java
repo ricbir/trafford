160fdcf1b3d4621b7c0f73fd2f4b7c5b0f00ef6b
Riccardo Biraghi <ricbir@gmail.com>
2019-03-10 16:13:36 +0000

    Use JavaFX properties for UI fields in controller

--- src/main/java/uk/ac/manchester/trafford/ApplicationController.java
+++ src/main/java/uk/ac/manchester/trafford/ApplicationController.java
@@ -4,22 +4,25 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.animation.AnimationTimer;
-import javafx.event.ActionEvent;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextInputControl;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
+import javafx.util.converter.NumberStringConverter;
 import uk.ac.manchester.trafford.network.RoadNetwork;
 import uk.ac.manchester.trafford.network.RoadNetworkBuilder;
 import uk.ac.manchester.trafford.network.edge.Edge;
 import uk.ac.manchester.trafford.network.edge.EdgePosition;
 
-public class ApplicationController {
+public class ApplicationController implements Initializable {
 
 	@FXML
 	private ResourceBundle resources;
@@ -43,56 +46,28 @@ public class ApplicationController {
 	private RoadNetwork network;
 
 	@FXML
+	private TextField nAgentsField;
+
+	@FXML
+	private TextField spawnRateField;
+
+	@FXML
+	private TextField agentSpeedField;
+
+	@FXML
+	private TextField agentSpeedVariabilityField;
+
+	private IntegerProperty agentNumber = new SimpleIntegerProperty(1);
+	private IntegerProperty agentSpawnRate = new SimpleIntegerProperty(1);
+
+	private double lastMouseX = 0;
+	private double lastMouseY = 0;
+
+	@FXML
 	void onScroll(ScrollEvent event) {
 		double scrollAmount = event.getDeltaY();
 		renderer.setScalingFactor(renderer.getScalingFactor() + (scrollAmount * 0.00005));
 	}
-
-	private int agentNumber = 1000;
-	private int agentSpawnRate = 5;
-
-	@FXML
-	void onAgentNumberChanged(ActionEvent event) {
-		TextInputControl field = (TextField) event.getSource();
-		try {
-			agentNumber = Integer.parseInt(field.getText());
-		} catch (NumberFormatException e) {
-			field.setText(Integer.toString(agentNumber));
-		}
-	}
-
-	@FXML
-	void onAgentSpawnRateChanged(ActionEvent event) {
-		TextInputControl field = (TextField) event.getSource();
-		try {
-			agentSpawnRate = Integer.parseInt(field.getText());
-		} catch (NumberFormatException e) {
-			field.setText(Integer.toString(agentSpawnRate));
-		}
-	}
-
-	@FXML
-	void onAgentSpeedChanged(ActionEvent event) {
-		TextInputControl field = (TextField) event.getSource();
-		try {
-			network.setAgentSpeed(Double.parseDouble(field.getText()));
-		} catch (NumberFormatException e) {
-			field.setText(Double.toString(network.getAgentSpeed()));
-		}
-	}
-
-	@FXML
-	void onAgentSpeedVariabilityChanged(ActionEvent event) {
-		TextInputControl field = (TextField) event.getSource();
-		try {
-			network.setAgentSpeedVariability(Double.parseDouble(field.getText()));
-		} catch (NumberFormatException e) {
-			field.setText(Double.toString(network.getAgentSpeedVariability()));
-		}
-	}
-
-	private double lastMouseX = 0;
-	private double lastMouseY = 0;
 
 	@FXML
 	void onMouseDragged(MouseEvent event) {
@@ -108,8 +83,27 @@ public class ApplicationController {
 		lastMouseY = event.getY();
 	}
 
-	@FXML
-	void initialize() {
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Bindings.bindBidirectional(nAgentsField.textProperty(), agentNumber, new NumberStringConverter());
+		Bindings.bindBidirectional(spawnRateField.textProperty(), agentSpawnRate, new NumberStringConverter());
+
+		agentSpeedField.textProperty().addListener((observable, oldValue, newValue) -> {
+			try {
+				network.setAgentSpeed(Double.parseDouble(newValue));
+			} catch (NumberFormatException e) {
+				((TextField) observable).setText(oldValue);
+			}
+		});
+
+		agentSpeedVariabilityField.textProperty().addListener((observable, oldValue, newValue) -> {
+			try {
+				network.setAgentSpeedVariability(Double.parseDouble(newValue));
+			} catch (NumberFormatException e) {
+				((TextField) observable).setText(oldValue);
+			}
+		});
+
 		simulationCanvas.heightProperty().bind(simulationPane.heightProperty());
 		simulationCanvas.widthProperty().bind(simulationPane.widthProperty());
 
@@ -131,8 +125,8 @@ public class ApplicationController {
 					while (now - lastUpdateTime > (timeBetweenUpdates = (long) (Constants.NANOSECONDS_PER_SECOND
 							/ Constants.UPDATES_PER_SECOND / speedSlider.getValue()))) {
 						if (runButton.isSelected()) {
-							for (int i = 0; (i < Math.min(agentNumber - network.getNumberOfAgents(),
-									agentSpawnRate)); i++) {
+							for (int i = 0; (i < Math.min(agentNumber.get() - network.getNumberOfAgents(),
+									agentSpawnRate.get())); i++) {
 								edges = network.edgeSet().toArray(edges);
 								int startEdgeIndex = (int) (Math.random() * edges.length);
 								int targetEdgeIndex;
